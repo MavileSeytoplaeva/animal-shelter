5db881708cbbc25fd0311db53e0b8a6a2822e670
Мавиле Сейтоплаева <mavile.seitoplaeva@gmail.com>
2023-10-26 12:56:21 +0300

    2. Переделала метод и добавила BotCommand

--- src/main/java/com/shelter/animalshelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/shelter/animalshelter/listener/TelegramBotUpdatesListener.java
@@ -2,8 +2,13 @@ package com.shelter.animalshelter.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.botcommandscope.BotCommandScope;
+import com.pengrad.telegrambot.model.botcommandscope.BotCommandScopeDefault;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.request.SetMyCommands;
+import com.pengrad.telegrambot.response.BaseResponse;
 import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +22,10 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
@@ -42,15 +50,47 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             long chatId = update.message().chat().id();
             String userName = update.message().chat().firstName();
 //            Проверяю если получили сообщение /start
-            if (update.message().text() != null && update.message().text().equals("/start")) {
-//                Отправляю приветственное сообщение
-                SendMessage messageText = new SendMessage(chatId, "Привет я Бот, который поможет тебе обрести лучшего друга в лице животного. Пожалуйста выбери из списка приют, который тебе нужен. /cats чтобы узнать больше о приюте для кошек и /dogs чтобы узнать больше о приюте для собак.");
-                SendResponse response = bot.execute(messageText);
+            switch (update.message().text()) {
+                case "/start" :
+                    SendMessage messageText = new SendMessage(chatId, "Привет я Бот, который поможет тебе обрести лучшего друга в лице животного. Пожалуйста выбери из списка приют, который тебе нужен.");
+                    SendResponse response = bot.execute(messageText);
+                    commandShelterList(chatId);
+
 
             }
 
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+    private void commandShelterList(long chatId){
+        List<BotCommand> botCommandList = new ArrayList<>(List.of(
+                new BotCommand("/cats", "Приют для кошек"),
+                new BotCommand("/dogs", "Приют для собак")
+        ));
+        BotCommand[] botCommands = botCommandList.toArray(new BotCommand[0]);
+        SetMyCommands myCommands = new SetMyCommands(botCommands);
+
+        BaseResponse response = bot.execute(myCommands);
+        if (response.isOk()) {
+            System.out.println("Команды успешно установлены!");
+            // Текст сообщения с командами
+            StringBuilder messageText = new StringBuilder();
+            for (BotCommand command : botCommandList) {
+                messageText.append(command.command()).append(" - ").append(command.description()).append("\n");
+            }
+            SendMessage sendMessage = new SendMessage(chatId, messageText.toString());
+            SendResponse sendResponse = bot.execute(sendMessage);
+        } else {
+            System.out.println("Ошибка установки команд: " + response.description());
+        }
+    }
+
+    private void introduceShelters(long chatId) {
+
+
+
+
     }
 }
 
